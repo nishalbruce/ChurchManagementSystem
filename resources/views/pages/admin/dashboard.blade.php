d3b7f09633570a9c0a55f866506f9ab91f89b29e
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2022-08-21 01:42:37 +0200

    created dashboard file

---
+++ resources/views/pages/admin/dashboard.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tableau de Bord - SGE</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="icon" type="image/x-icon"
+		href="../resources/img/logo-biu.png" />
+	<!-- Font Awesome -->
+	<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+		rel="stylesheet" />
+	<!-- MDB -->
+	<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.1.0/mdb.min.css"
+		rel="stylesheet" />
+</head>
+
+<body>
+
+
+	<!-- MDB -->
+	<script type="text/javascript"
+		src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.1.0/mdb.min.js"></script>
+</body>
+</html>
+
